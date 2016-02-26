ba145060fb86e16d766334c79dd5d6132741bb17
jareda <jareda@localhost>
2016-02-26 16:29:37 -0500

    Finished Project.

--- TestOneJSA/src/saintjoe/edu/cs/jareda/testonejsa/TestOneJSA.java
+++ TestOneJSA/src/saintjoe/edu/cs/jareda/testonejsa/TestOneJSA.java
@@ -4,7 +4,12 @@ public class TestOneJSA {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int i = 0;
+		while (i < 6) {
+		System.out.println("Jared Ast");
 
+		i = i + 1; }
+			
 	}
 
 }
